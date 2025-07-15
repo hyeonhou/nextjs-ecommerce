128bb6a33357ddce6c56a9053b47fc9ced9205c9
김현호 <hyeonhokim@naver.com>
2025-07-15 11:48:37 +0900

    250715 get & display users

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -13,6 +13,7 @@ import { prisma } from "@/db/prisma";
 import { formatError } from "../utils";
 import { ShippingAddress } from "@/types";
 import z from "zod";
+import { PAGE_SIZE } from "../constants";
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
@@ -181,4 +182,26 @@ export async function updateProfile(user: { name: string; email: string }) {
       message: formatError(error),
     };
   }
+}
+
+// Get all the users
+export async function getAllUsers({
+  limit = PAGE_SIZE,
+  page,
+}: {
+  limit?: number;
+  page: number;
+}) {
+  const data = await prisma.user.findMany({
+    orderBy: { createdAt: "desc" },
+    take: limit,
+    skip: (page - 1) * limit,
+  });
+
+  const dataCount = await prisma.user.count();
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  };
 }
