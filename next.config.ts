2340cd12e662ffb966913d5f0b5367449631917c
김현호 <hyeonhokim@naver.com>
2025-07-15 09:42:26 +0900

    250715 uploadthing configuration

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
+      },
+    ],
   },
 };
 
