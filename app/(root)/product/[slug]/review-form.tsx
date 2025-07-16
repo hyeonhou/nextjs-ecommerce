a9c245013fbebfd018419c19356e58c3412987de
김현호 <hyeonhokim@naver.com>
2025-07-16 10:28:03 +0900

    250716 review form dialog component

---
+++ app/(root)/product/[slug]/review-form.tsx
@@ -0,0 +1,148 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { reviewFormDefaultValues } from "@/lib/constants";
+import { insertReviewSchema } from "@/lib/validators";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { StarIcon } from "lucide-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import z from "zod";
+
+const ReviewForm = ({
+  userId,
+  productId,
+  onReviewSubmitted,
+}: {
+  userId: string;
+  productId: string;
+  onReviewSubmitted?: () => void;
+}) => {
+  const [open, setOpen] = useState(false);
+  const form = useForm<z.infer<typeof insertReviewSchema>>({
+    resolver: zodResolver(insertReviewSchema),
+    defaultValues: reviewFormDefaultValues,
+  });
+
+  const handleOpenForm = () => {
+    setOpen(true);
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <Button onClick={handleOpenForm} variant="default">
+        Write a Review
+      </Button>
+      <DialogContent className="sm:max-w-[425px]">
+        <Form {...form}>
+          <form method="post">
+            <DialogHeader>
+              <DialogTitle>Write a Review</DialogTitle>
+              <DialogDescription>
+                Share your thoughts with other customers
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Title</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter title" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Description</FormLabel>
+                    <FormControl>
+                      <Textarea placeholder="Enter description" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="rating"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Rating</FormLabel>
+                    <Select
+                      {...field}
+                      defaultValue={
+                        !field.value ? undefined : field.value.toString()
+                      }
+                      value={!field.value ? undefined : field.value.toString()}
+                      onValueChange={field.onChange}
+                    >
+                      <FormControl>
+                        <SelectTrigger className="w-full">
+                          <SelectValue placeholder="Select a rating" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {Array.from({ length: 5 }).map((_, index) => (
+                          <SelectItem
+                            key={index}
+                            value={(index + 1).toString()}
+                          >
+                            {index + 1} <StarIcon className="inline h-4 w-4" />
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter>
+              <Button
+                type="submit"
+                size="lg"
+                className="w-full mt-1"
+                disabled={form.formState.isSubmitting}
+              >
+                {form.formState.isSubmitting ? "Submitting..." : "Submit"}
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ReviewForm;
