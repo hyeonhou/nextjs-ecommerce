31af1be88db8b28077bce8f1f2562a5e5bc46960
김현호 <hyeonhokim@naver.com>
2025-07-16 11:28:46 +0900

    250716 display reviews

--- types/index.ts
+++ types/index.ts
@@ -13,6 +13,7 @@ import {
 export type Product = z.infer<typeof insertProductSchema> & {
   id: string;
   rating: string;
+  numReviews: number;
   createdAt: Date;
 };
 
@@ -31,10 +32,8 @@ export type Order = z.infer<typeof insertOrderSchema> & {
   user: { name: string; email: string };
 };
 export type PaymentResult = z.infer<typeof paymentResultSchema>;
-export type Review = z.infer<
-  typeof insertReviewSchema & {
-    id: string;
-    createdAt: Date;
-    user?: { name: string };
-  }
->;
+export type Review = z.infer<typeof insertReviewSchema> & {
+  id: string;
+  createdAt: Date;
+  user?: { name: string };
+};
