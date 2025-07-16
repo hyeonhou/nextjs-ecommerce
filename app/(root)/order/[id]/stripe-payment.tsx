a86958433d28bead63e059637f9420a662404d8d
김현호 <hyeonhokim@naver.com>
2025-07-16 13:10:34 +0900

    250716 order form payment intent

---
+++ app/(root)/order/[id]/stripe-payment.tsx
@@ -0,0 +1,13 @@
+const StripePayment = ({
+  priceInCents,
+  orderId,
+  clientSecret,
+}: {
+  priceInCents: number;
+  orderId: string;
+  clientSecret: string;
+}) => {
+  return <>Stripe form</>;
+};
+
+export default StripePayment;
